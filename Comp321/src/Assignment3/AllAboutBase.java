9cd340bb6a3576a6ca61266b4022a73845896e69
Bowen Tian <bowen.tian@mail.mcgill.ca>
2018-10-07 10:05:17 -0400

    All about that base

--- Comp321/src/Assignment3/AllAboutBase.java
+++ Comp321/src/Assignment3/AllAboutBase.java
@@ -1,51 +1,96 @@
 package Assignment3;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class AllAboutBase {
+    private final static BigInteger UpperBound = BigInteger.valueOf(2).pow(32).subtract(BigInteger.ONE);
+    private final static BigInteger LowerBound = BigInteger.ONE;
+	
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		int N  = sc.nextInt();
-		sc.nextLine();//get rid of this line
-		for(int i=0; i<N; i++){
-			processExp(sc.nextLine());
+		int N = sc.nextInt();
+		while (N-- > 0) {
+			String operant1 = sc.next();
+			String operator = sc.next();
+			String operant2 = sc.next();
+			sc.next();
+			String result = sc.next();
+			
+			processExp(operant1,operator,operant2,result);
 		}
 	}
 	
-	private static void processExp(String exp){
-		String[] token = exp.split(" ");
-		if(token.length > 4){
-			String operant1 = token[0];
-			String operator = token[1];
-			String operant2 = token[2];
-			String equalSign = token[3];
-			String result = token[4];
-			for(int i=getSmallestBasePossible(exp);i<=36; i++){
-				Long o1 = Long.parseLong(operant1, i);
-				Long o2 = Long.parseLong(operant2, i);
-				Long r = Long.parseLong(result, i);
-				if(operator.equals("+") && o1 + o2 == r){
-					System.out.println(" "+i);
+	private static boolean containsOneOnly(String exp) {
+		String newExp = exp.replaceAll("1", "");
+		return newExp.isEmpty();
+	}
+	
+	private static void processExp(String operant1, String operator,String operant2,String result){
+		boolean found = false;
+		for(int i=1;i<=36; i++){
+			BigInteger o1, o2, r;
+			if(i==1) {
+				if(!containsOneOnly(operant1) || !containsOneOnly(operant2) || !containsOneOnly(result)) {
+					continue;
 				}
-				else if(operator.equals("-") && o1-o2 == r){
-					
-				}
-				else if(operator.equals("*") && o1*o2 == r){
-					
-				}
-				else if(operator.equals("/") && o1/o2 == r){
-					
-				}
+				o1 = BigInteger.valueOf(operant1.length());
+				o2 = BigInteger.valueOf(operant2.length());
+				r = BigInteger.valueOf(result.length());
 			}
+			else {
+				if(i < getSmallestBasePossible(operant1,operant2,result)) {
+					continue;
+				}
+				o1 = new BigInteger(operant1,i);
+				o2 = new BigInteger(operant2,i);
+				r = new BigInteger(result,i);
+			}
+				
+			if(!isValid(o1) || !isValid(o2) || !isValid(r)) {
+				continue;
+			}
+			
+			if( (operator.equals("+") && o1.add(o2).compareTo(r) == 0) 
+					|| (operator.equals("-") && o1.subtract(o2).compareTo(r) == 0)
+					||(operator.equals("*") && o1.multiply(o2).compareTo(r) == 0)
+					||(operator.equals("/") && o1.divideAndRemainder(o2)[1].compareTo(BigInteger.ZERO)==0
+					&& o1.divideAndRemainder(o2)[0].compareTo(r) == 0)){
+				printBase(i);
+				found = true;
+			}
+		}
+		if(!found) {
+			System.out.println("invalid");
+		}
+		else {
+			System.out.println();
+		}
+	}
+	
+	private static boolean isValid(BigInteger number) {
+		return number.compareTo(UpperBound) <= 0 && number.compareTo(LowerBound) >= 0;
+	}
+	
+	private static void printBase(int base) {
+		if(base < 10) {
+			System.out.print(base);
+		}
+		else if(base == 36) {
+			System.out.println(0);
+		}
+		else {
+			System.out.print((char)(base+87));
 		}
 	}
 	
 	/*
 	 * Verified
 	 */
-	private static int getSmallestBasePossible(String exp){
+	private static int getSmallestBasePossible(String operant1, String operant2,String result){
+		String exp = operant1 + operant2 + result;
 		int max = 0;//the minimum is 42 so 0 is ok
 		for(int i=0; i< exp.length(); i++){
 			int cur = exp.charAt(i);
@@ -54,13 +99,16 @@ public class AllAboutBase {
 			}
 			max = Math.max(cur,max);
 		}
-		if(max >= 48 && max <= 57){
+		if(max<49) {
+			return 1;
+		}
+		if(max >= 49 && max <= 57){
 			return max-47;
 		}
 		if(max >= 97 && max <= 122){
 			return max-86;
 		}
-	return 0;
+	return 100;
 		
 	}
 }
---
+++ Comp321/src/Other/TesterClass.java
@@ -0,0 +1,7 @@
+package Other;
+
+public class TesterClass {
+	public static void main(String[] args) {
+		System.out.println((char)('z'+4));
+	}
+}
